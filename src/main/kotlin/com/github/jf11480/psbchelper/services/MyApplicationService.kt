e15ac27b68a745a40fd1655857045499fb80869c
GitHub Action <action@github.com>
2022-08-13 12:34:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/jf11480/psbchelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jf11480/psbchelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jf11480.psbchelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jf11480.psbchelper.MyBundle
 
 class MyApplicationService {
 
